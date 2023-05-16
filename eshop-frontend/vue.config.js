0918b675780a8cae71d5fa462af864e65bb0887a
kodiss <kodiss@naver.com>
2023-05-16 13:52:22 +0900

    modify

--- eshop-frontend/vue.config.js
+++ eshop-frontend/vue.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   // Local Legacy
-  devServer: {
-    proxy: {
-        "^/": {
-          target: "http://localhost:8090"
-        }
-    },
-    disableHostCheck: true
-  }
+  // devServer: {
+  //   proxy: {
+  //       "^/": {
+  //         target: "http://localhost:8090"
+  //       }
+  //   },
+  //   disableHostCheck: true
+  // }
 
   // EC2 OR Dockerize & skaffold
   // devServer: {
